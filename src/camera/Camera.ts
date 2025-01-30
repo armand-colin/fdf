f8a7b73f943f679bd95330a0062705c6173edfbe
armand-colin <56818352+armand-colin@users.noreply.github.com>
2025-01-30 17:32:56 +0100

    added identity

--- src/camera/Camera.ts
+++ src/camera/Camera.ts
@@ -2,6 +2,6 @@ import { Mat4 } from "../math/Mat4"
 
 export class Camera {
 
-    readonly matrix = new Mat4()
+    readonly matrix = Mat4.identity()
 
 }
--- src/math/Mat4.ts
+++ src/math/Mat4.ts
@@ -2,7 +2,16 @@ export class Mat4 {
 
     readonly buffer: Float32Array
     readonly size = 16
-    
+
+    static identity() {
+        return new Mat4(new Float32Array([
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]))
+    }
+
     constructor(buffer?: Float32Array) {
         if (!buffer)
             buffer = new Float32Array(16)
